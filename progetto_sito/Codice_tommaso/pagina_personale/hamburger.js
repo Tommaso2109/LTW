f237df5b8bb57d037b496a60689137a9d6e53309
Tommaso Rinaldi <162431612+Tommaso2109@users.noreply.github.com>
2024-04-19 21:51:18 +0100

    progetto

---
+++ progetto_sito/Codice_tommaso/pagina_personale/hamburger.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    /*open panel*/
+    $(".hamburger").on('click', function(){
+        $(".menu").toggleClass("menu--open");
+        
+    });
+});
+// Supponiamo che questa funzione ritorni true se l'utente è loggato, false altrimenti
+function isUserLoggedIn() {
+    // Implementa la logica di controllo dell'autenticazione qui
+    //return localStorage.getItem('username') !== null;
+    return true;
+}
+
+$(document).ready(function() {
+    if (isUserLoggedIn()) {
+        // Nascondi i pulsanti di login e registrazione
+        $("#loginButton, #registerButton").hide();
+
+        // Mostra l'immagine dell'utente e rendila cliccabile
+        $("#userImage").show().css('cursor', 'pointer');
+    }
+});
--- progetto_sito/Lavori_finiti/hamburger.js
+++ progetto_sito/Lavori_finiti/hamburger.js
@@ -6,3 +6,23 @@ $(document).ready(function(){
         
     });
 });
+// Supponiamo che questa funzione ritorni true se l'utente è loggato, false altrimenti
+function isUserLoggedIn() {
+    // Implementa la logica di controllo dell'autenticazione qui
+    //return localStorage.getItem('username') !== null;
+    return true;
+}
+
+$(document).ready(function() {
+    if (isUserLoggedIn()) {
+        // Nascondi i pulsanti di login e registrazione
+        $("#loginButton, #registerButton").hide();
+
+        // Mostra l'immagine dell'utente e rendila cliccabile
+       $("#userImage").show().css('cursor', 'pointer')
+       $("#userImage > img").css({
+        'width': '100px',  // Imposta la larghezza desiderata
+        'height': 'auto'  // Mantieni le proporzioni originali
+    });
+    }
+});

07e7b2e9dd03f72447aa472c15732027e6500afb
AdviceLOL <dationeris@gmail.com>
2024-04-26 22:50:09 +0200

    amici

--- progetto_sito/Lavori_finiti/salva.php
+++ progetto_sito/Lavori_finiti/salva.php
@@ -11,6 +11,7 @@
         
     }
 
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $scuderia = $_POST['scuderia'];
         $pilota1 = $_POST['pilota1'];
@@ -18,33 +19,80 @@
 
         $utente = $_SESSION['username'];
 
-        // Controlla se l'utente esiste già
-        $checkSql = "SELECT * FROM squadra WHERE utente = ?";
-        $checkStmt = $conn->prepare($checkSql);
-        $checkStmt->bind_param("s", $utente);
-        $checkStmt->execute();
-        $checkResult = $checkStmt->get_result();
+        //Punteggi
+        $puntiPilota1Gara = "0";
+        $puntiPilota2Gara = "0";
+        $moltiplicatoreScuderiaGara = "1";
 
-        if ($checkResult->num_rows > 0) {
-            // La squadra esiste già, invia una risposta appropriata
-            echo json_encode(["error" => "La squadra è già stata fatta."]);
-        } else {
-            // La squadra non esiste, inserisci i dati
-            $sql = "INSERT INTO squadra (utente,scuderia, pilota1, pilota2) VALUES (? ,?, ?, ?)";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("ssss", $utente, $scuderia, $pilota1, $pilota2);
+        $sql = "SELECT posizione, nome, scuderia, fastLap FROM ultimagara";
+        $result = $conn->query($sql); 
+        if ($result->num_rows > 0) {    
+            $puntixposizione = array(25, 18, 15, 12, 10, 8, 6, 4, 2, 1);
+            $moltiplicatoreScuderia = array(4 , 3, 2, 1.75, 1.35, 1.30, 1.25, 1.20, 1.15, 1.10);
+            // Stampa i dati di ogni riga
+            
+            $found = false;
+            while($row = $result->fetch_assoc()){
+                // Assegna i dati alle variabili
+                $posizione = $row["posizione"];
+                $nome = $row["nome"];
+                $scuderiaPilota = $row["scuderia"];
+                $fastLap = $row["fastLap"];
+                
+                if($nome == $pilota1 || $nome == $pilota2){
+                    if($nome == $pilota1){
+                        $puntiPilota1Gara += $puntixposizione[$posizione-1];
+                        if($fastLap)$puntiPilota1Gara += 3;
+                    }else if($nome == $pilota2){
+                        $puntiPilota2Gara += $puntixposizione[$posizione-1];
+                        if($fastLap)$puntiPilota2Gara += 3;
+                    }
+                }
+                if(!$found && $scuderiaPilota == $scuderia){
+                    $moltiplicatoreScuderiaGara = $moltiplicatoreScuderia[$posizione-1];
+                    $found = true;
+                } 
+            } 
 
-            if($stmt->execute()) {
-                echo json_encode(["success" => "Squadra salvata con successo."]);
+            $puntiPilota1Gara *= $moltiplicatoreScuderiaGara;
+            $puntiPilota2Gara *= $moltiplicatoreScuderiaGara;
+
+            $punteggioTotale = $puntiPilota1Gara + $puntiPilota2Gara;
+        try {
+            // Controlla se l'utente esiste già
+            $checkSql = "SELECT * FROM squadra WHERE utente = ?";
+            $checkStmt = $conn->prepare($checkSql);
+            $checkStmt->bind_param("s", $utente);
+            $checkStmt->execute();
+            $checkResult = $checkStmt->get_result();
+
+            if ($checkResult->num_rows > 0) {
+                // La squadra esiste già, invia una risposta appropriata
+                echo json_encode(["error" => "La squadra è già stata fatta."]);
             } else {
-                echo json_encode(["error" => "Errore: " . $stmt->error]);
-            }
+                // La squadra non esiste, inserisci i dati
+                $sql = "INSERT INTO squadra (utente,scuderia, pilota1, pilota2, punteggioTotale) VALUES (? ,?, ?, ?, ?)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("ssssi", $utente, $scuderia, $pilota1, $pilota2, $punteggioTotale);
+            
+                if($stmt->execute()) {
+                    echo json_encode(["success" => "Squadra salvata con successo."]);
+                } else {
+                    echo json_encode(["error" => "Errore: " . $stmt->error]);
+                }
+                 
+                }
+        }catch (Exception $e) {
+            echo json_encode(["error" => "Errore inaspettato: " . $e->getMessage()]);
+            exit();
         }
+              
         } else {
             echo json_encode(["error" => "No POST data received."]);
         }
 
-    
+    }
+
 ?>
 <script>
     
